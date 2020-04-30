602452a096fee9d87a1e1c3a1f29eb8542ea099a
Kan Yu <WalterYeYint@gmail.com>
2020-04-30 18:11:51 +0630

    Changed the data sent from int to float in PID-Test.py. Changed .toInt() to .toFloat() and datatype of data[3] to float in PID-Test.ino. And other minor changes.

--- PID-Test/PID-Test/PID-Test.ino
+++ PID-Test/PID-Test/PID-Test.ino
@@ -3,13 +3,11 @@
 
 #include <PID_v1.h>
 
-unsigned int MAX_DISTANCE = 400;
-
 //PID ideal values
 //value 1 ==> Kp = 6.5, Ki = 0, Kd = 1.1
 
 String r;
-int data[3];
+float data[3];
 
 long duration, cm = 0;
 //float Kp = 15.1;        //2.5 = default, 6.5 = perfect, 26.5 = shakin                                              //Initial Proportional Gain
@@ -30,7 +28,7 @@ void setup() {
   Input = cm;       
  
   myPID.SetMode(AUTOMATIC);                                          //Set PID object myPID to AUTOMATIC 
-  myPID.SetOutputLimits(-10, 10);                                     //Set Output limits to -80 and 80 degrees. 
+  myPID.SetOutputLimits(0, 100);                                     //Set Output limits to -80 and 80 degrees. 
 }
 
 void loop()
@@ -38,20 +36,29 @@ void loop()
   if(Serial.available()){ 
     for(int i=0; i<3; i++){
       r = (Serial.readStringUntil('\t'));  //conveting the value of chars to integer
-      data[i] = r.toInt();
+      data[i] = r.toFloat();
 //      Serial.print(data[i]);
 //      Serial.print('\t');
     }
     Kp = data[0];
     Ki = data[1];
     Kd = data[2];
+    myPID.SetTunings(Kp, Ki, Kd);
   }
-  Setpoint = 50;
+  Setpoint = 100;
   Input = cm;     
   myPID.Compute();
-  Serial.print(cm);
-  Serial.print(" ");     
-  Serial.println(Setpoint);
+//  Serial.print(cm);
+//  Serial.print(" ");     
+//  Serial.print(Setpoint);
+//  Serial.print(" "); 
+//  Serial.print(Output);
+//  Serial.println("");
+    Serial.print(Kp);
+    Serial.print(" ");
+    Serial.print(Ki);
+    Serial.print(" ");
+    Serial.println(Kd);
   cm = cm + Output;                              
   
   
