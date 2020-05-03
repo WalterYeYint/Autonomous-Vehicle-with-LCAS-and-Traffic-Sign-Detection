8fa5116692a1866324e16a67bccb72edf2376086
Kan Yu <WalterYeYint@gmail.com>
2020-05-04 02:03:44 +0630

    Changed detecting_edges_grayscale to detecting_contour func. Changed the white line detecting method from HSV to grayscale with dilate,erode, and others (thus reducing the noise). Still need to fix the error output part, it's still inaccurate.

--- filter_test.py
+++ filter_test.py
@@ -2,20 +2,19 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def detecting_edges_grayscale(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    kernel = 5
-    blur = cv2.GaussianBlur(gray,(kernel, kernel),0)
-    canny = cv2.Canny(gray, 50, 150)
-    return canny
+def detecting_contour(img):
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
 
 def detecting_edges(img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    cv2.imshow("hsv image", hsv)
+    # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # cv2.imshow("hsv image", hsv)
 
-    lower_white = np.array([0, 0, 150])
-    upper_white = np.array([180, 70, 255])
-    mask = cv2.inRange(hsv, lower_white, upper_white)
+    # lower_white = np.array([0, 0, 200])
+    # upper_white = np.array([180, 100, 255])
+    # mask = cv2.inRange(hsv, lower_white, upper_white)
 
     # lower_red = np.array([170, 100, 0])
     # upper_red = np.array([180, 255, 255])
@@ -25,10 +24,15 @@ def detecting_edges(img):
     # upper_blue = np.array([150, 255, 255])
     # mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-    # #Convert to grayscale
-    # gray = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
+    # #Convert to grayscale then dilate and erode
+    kernel = np.ones((3,3), np.uint8)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    mask = cv2.inRange(gray, 210, 255)
+    mask = cv2.erode(mask, kernel, iterations=5)
+    mask = cv2.dilate(mask, kernel, iterations=9)
 
-    cv2.imshow("mask image", mask)
+    #cv2.imshow("mask image", mask)
+
     #
     # kernel = 5
     # #Gaussian Blur for smoothing (otherwise, the picture could bring false edges)
@@ -36,7 +40,7 @@ def detecting_edges(img):
     # Canny(img, lower_threshold, upper_threshold)
     #Draws edge if beyond upper_threshold, not if below lower_threshold, draws only if edge is connected to strong edge if between
     canny = cv2.Canny(mask, 50, 150)
-    return canny
+    return mask
 
 def region_of_interest(canny):
     height = canny.shape[0]
@@ -62,7 +66,7 @@ def detect_line_segments(cropped_edges):
     # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
     rho = 1  # distance precision in pixel, i.e. 1 pixel
     angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
-    min_threshold = 90  # minimal of votes
+    min_threshold = 130  # minimal of votes
     line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold,
                                     np.array([]), minLineLength=8, maxLineGap=4)
     return line_segments
@@ -108,6 +112,35 @@ def average_slope_intercept(frame, line_segments):
 
     return lane_lines
 
+def average_slope_intercept_middle_line(frame, line_segments):
+    lane_lines = []
+    if line_segments is None:
+        #logging.info('No line_segment segments detected')
+        return lane_lines
+
+    height, width, _ = frame.shape
+    middle_fit = []
+
+    boundary = 1/3
+
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:
+            if x1 == x2:
+                #logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                continue
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            middle_fit.append((slope, intercept))
+
+    middle_fit_average = np.aveerage(middle_fit, axis=0)
+    if len(middle_fit) > 0:
+        lane_lines.append(make_coordinates(frame, middle_fit_average))
+
+    #logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+
+    return lane_lines
+
 def make_coordinates(image, line_parameters):
     height, width, _ = image.shape
     if isinstance(line_parameters, np.float64):
@@ -123,11 +156,13 @@ def make_coordinates(image, line_parameters):
     x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
     return [[x1, y1, x2, y2]]
 
+
+
 def detect_lane(lane_image):
     canny_image = detecting_edges(lane_image)
     cropped_image = region_of_interest(canny_image)
     lines = detect_line_segments(cropped_image)
-    averaged_lines = average_slope_intercept(lane_image, lines)
+    averaged_lines = average_slope_intercept_middle_line(lane_image, lines)
     return averaged_lines
 
 def display_lines(frame, lines, line_color=(0, 255, 0), line_width=2):
@@ -140,32 +175,68 @@ def display_lines(frame, lines, line_color=(0, 255, 0), line_width=2):
     return line_image
 
 def calculate_offset(frame, line_segments):
-    height, width, _ = frame.shape
-    setpoint = width / 2
-    for line_segment in line_segments:
-        for x1, y1, x2, y2 in line_segment:
-            offset = setpoint - x1
-    return offset
+    contours_blk, hierarchy_blk = cv2.findContours(Blackline.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(contours_blk) > 0:    
+        blackbox = cv2.minAreaRect(contours_blk[0])
+        (x_min, y_min), (w_min, h_min), ang = blackbox
+
+def test_video(video_file):
+    cap = cv2.VideoCapture(video_file)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            #contour_image = detecting_contour(frame)
+            canny_image = detecting_edges(frame)
 
 
-#image = cv2.imread('test_lane.jpeg')
-image = cv2.imread('black_white_line.jpg')
-image_re = cv2.resize(image, (960, 721))
-lane_image = np.copy(image_re)
+            contours_blk, hierarchy_blk = cv2.findContours(canny_image.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            if len(contours_blk) > 0:    
+                blackbox = cv2.minAreaRect(contours_blk[0])
+                (x_min, y_min), (w_min, h_min), ang = blackbox
+            height, width, _ = frame.shape
+            setpoint = width / 2
+            error = int(x_min - setpoint)
+            cv2.putText(canny_image,str(error),(10, 320), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            cv2.line(canny_image, (int(x_min),200 ), (int(x_min),250 ), (255,0,0),3)
+
+            #cropped_image = region_of_interest(canny_image)
+            # lines = detect_line_segments(cropped_image)
+            # averaged_lines = average_slope_intercept(frame, lines)
+            # lane_lines_image = display_lines(frame, averaged_lines)
+            cv2.imshow("result", canny_image)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
 
-# averaged_lines = detect_lane(lane_image)
-# print(averaged_lines)
-# lane_lines_image = display_lines(lane_image, averaged_lines)
-# cv2.imshow("lane lines", lane_lines_image)
-# cv2.waitKey(0)
+def test_photo(photo_file):
+    #image = cv2.imread('test_lane.jpeg')
+    image = cv2.imread(photo_file)
+    image_re = cv2.resize(image, (960, 721))
+    lane_image = np.copy(image_re)
 
 
-######################################################################################
-#For testing bit by bit
+    # averaged_lines = detect_lane(lane_image)
+    # print(averaged_lines)
+    # lane_lines_image = display_lines(lane_image, averaged_lines)
+    # cv2.imshow("lane lines", lane_lines_image)
+    # cv2.waitKey(0)
 
-canny_image = detecting_edges(lane_image)
-#canny_image = detecting_edges_grayscale(lane_image)
-cv2.imshow("original", image_re)
-cv2.imshow("lane lines", canny_image)
-cv2.waitKey(0)
+
+    ######################################################################################
+    #For testing bit by bit
+
+    canny_image = detecting_edges(lane_image)
+    #canny_image = detecting_edges_grayscale(lane_image)
+    cv2.imshow("original", image_re)
+    cv2.imshow("lane lines", canny_image)
+    cv2.waitKey(0)
+
+
+#test_photo('test_lane.jpeg')
+#test_photo('black_white_line.jpg')
+test_video('white_line.mp4')
