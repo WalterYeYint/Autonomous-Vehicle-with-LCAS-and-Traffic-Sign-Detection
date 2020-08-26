79bf142a1c3777ffaea3da5b6588b52ae5e00676
Kan Yu <WalterYeYint@gmail.com>
2020-08-26 14:08:44 +0630

    Successfully displayed the midpoints of lanes using calc_midpoints(). Deleted the unnecessary lines.

--- filter_test_main.py
+++ filter_test_main.py
@@ -126,39 +126,6 @@ def find_contours(image, lane_image):
     # cv2.imshow('test',lane_image)
     return box_dim
 
-def calc_midpoints(box_dim, frame):
-    box_center = []
-    height, width, _ = frame.shape
-    for box in box_dim:
-        moment = cv2.moments(box)
-        if (moment["m00"] != 0):
-            # Get the center x.
-            cx = int(moment["m10"]/moment["m00"])
-            # Get the center y.
-            cy = int(moment["m01"]/moment["m00"])
-        else:
-            cx, cy = 0, 0
-        box_center.append([cx, cy])
-
-        cv2.circle(frame, (cx, cy), 5, (0, 0, 255), 3)
-        box = np.array(box).reshape((-1,1,2)).astype(np.int32)          #This line is needed to drawContours the box
-        cv2.drawContours(frame, [box], -1, (0, 0, 255), 3) 
-    
-    if(box_center >= 2):
-        center_pt = int((box_center[0][0] + box_center[1][0])/2)
-    else:
-        if(box_center[0][0] >= width):
-            center_pt = int((box_center[0][0] + width)/2)
-        else:
-            center_pt = int((box_center[0][0] + width)/2)
-        
-    cv2.circle(frame, (center_pt, cy), 5, (0, 0, 255), 3)
-
-    # print(box_center[0][0])
-    cv2.imshow('test',frame)
-    
-    return box_center
-
 def sort_contours(box_dim, frame):
     left_box = []
     right_box = []
@@ -185,12 +152,12 @@ def sort_contours(box_dim, frame):
     if(len(left_box) > 0):
         left_box = max(left_box, key = cv2.contourArea)
         left_box = np.array(left_box).reshape((-1,1,2)).astype(np.int32)            #This line is needed to drawContours the box
-        cv2.drawContours(frame, [left_box], -1, (0, 0, 255), 3) 
+        # cv2.drawContours(frame, [left_box], -1, (0, 0, 255), 3) 
     if(len(right_box) > 0):
         right_box = max(right_box, key = cv2.contourArea)
         right_box = np.array(right_box).reshape((-1,1,2)).astype(np.int32)          #This line is needed to drawContours the box
-        cv2.drawContours(frame, [right_box], -1, (0, 0, 255), 3) 
-
+    #     cv2.drawContours(frame, [right_box], -1, (0, 0, 255), 3) 
+    # cv2.imshow('test',frame)
     
 
     # print("Left box=")
@@ -198,227 +165,50 @@ def sort_contours(box_dim, frame):
     # print("Right box=")
     # print(right_box)
 
-    cv2.imshow('test',frame)
+    
     return left_box, right_box
 
-
-def detect_line_segments(cropped_edges):
-    # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
-    rho = 1  # precision in pixel, i.e. 1 pixel
-    angle = np.pi / 180  # degree in radian, i.e. 1 degree
-    min_threshold = 40  # minimal of votes
-    line_segments = cv2.HoughLinesP(cropped_edges, cv2.HOUGH_PROBABILISTIC, angle, min_threshold, np.array([]), minLineLength=8,
-                                    maxLineGap=4)
-    return line_segments
-
-def average_slope_intercept(frame, line_segments):
-    lane_lines = []
-    if line_segments is None:
-        #logging.info('No line_segment segments detected')
-        return lane_lines
-
+def calc_midpoints(left_box, right_box, frame):
+    box_center = []
     height, width, _ = frame.shape
-    left_fit = []
-    right_fit = []
 
-    boundary = 1/3
-    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
-    right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
-
-    for line_segment in line_segments:
-        for x1, y1, x2, y2 in line_segment:
-            if x1 == x2:
-                #logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
-                continue
-            fit = np.polyfit((x1, x2), (y1, y2), 1)
-            slope = fit[0]
-            intercept = fit[1]
-            if slope < 0:
-                if x1 < left_region_boundary and x2 < left_region_boundary:
-                    left_fit.append((slope, intercept))
-            else:
-                if x1 > right_region_boundary and x2 > right_region_boundary:
-                    right_fit.append((slope, intercept))
-
-    left_fit_average = np.average(left_fit, axis=0)
-    if len(left_fit) > 0:
-        lane_lines.append(make_coordinates(frame, left_fit_average))
-
-    right_fit_average = np.average(right_fit, axis=0)
-    if len(right_fit) > 0:
-        lane_lines.append(make_coordinates(frame, right_fit_average))
-
-    #logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
-
-    return lane_lines
-
-def average_slope_intercept_middle_line(frame, line_segments):
-    lane_lines = []
-    if line_segments is None:
-        #logging.info('No line_segment segments detected')
-        return lane_lines
-
-    height, width, _ = frame.shape
-    middle_fit = []
-
-    boundary = 1/3
-
-    for line_segment in line_segments:
-        for x1, y1, x2, y2 in line_segment:
-            if x1 == x2:
-                #logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
-                continue
-            fit = np.polyfit((x1, x2), (y1, y2), 1)
-            slope = fit[0]
-            intercept = fit[1]
-            middle_fit.append((slope, intercept))
-
-    middle_fit_average = np.average(middle_fit, axis=0)
-    if len(middle_fit) > 0:
-        lane_lines.append(make_coordinates(frame, middle_fit_average))
-
-    #logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
-
-    return lane_lines
-
-def make_coordinates(image, line_parameters):
-    height, width, _ = image.shape
-    if isinstance(line_parameters, np.float64):
-    	slope = 0.0001
-    	intercept = 0.00
+    left_moment = cv2.moments(left_box)
+    right_moment = cv2.moments(right_box)
+    if (left_moment["m00"] != 0):
+        # Get the center x.
+        left_cx = int(left_moment["m10"]/left_moment["m00"])
+        # Get the center y.
+        left_cy = int(left_moment["m01"]/left_moment["m00"])
     else:
-        slope, intercept = line_parameters
-    y1 = height  # bottom of the frame
-    y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
-
-    # bound the coordinates within the frame
-    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
-    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
-    return [[x1, y1, x2, y2]]
-
-
-
-def detect_lane(lane_image):
-    canny_image = detecting_edges(lane_image)
-    cropped_image = region_of_interest(canny_image)
-    lines = detect_line_segments(cropped_image)
-    averaged_lines = average_slope_intercept(lane_image, lines)
-    lane_lines_image = display_lines(lane_image, averaged_lines)
-    return averaged_lines, lane_lines_image
-
-def calculate_offset(frame, line_segments):
-    contours_blk, hierarchy_blk = cv2.findContours(Blackline.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-    if len(contours_blk) > 0:    
-        blackbox = cv2.minAreaRect(contours_blk[0])
-        (x_min, y_min), (w_min, h_min), ang = blackbox
-
-def steer(frame, lane_lines, curr_steering_angle):
-        if len(lane_lines) == 0:
-            # logging.error('No lane lines detected, nothing to do.')
-            return frame, curr_steering_angle
-
-        new_steering_angle = compute_steering_angle(frame, lane_lines)
-        curr_steering_angle = stabilize_steering_angle(curr_steering_angle, new_steering_angle, len(lane_lines))
-        cv2.putText(frame,'Steering Angle: {0:.2f}'.format(curr_steering_angle),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
-        curr_heading_image = display_heading_line(frame, curr_steering_angle)
-        # cv2.putText(frame,'STA: {0:.2f}'.format(new_steering_angle),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
-        # curr_heading_image = display_heading_line(frame, new_steering_angle)
-
-        #cv2.imshow("heading", curr_heading_image)
-
-        return curr_heading_image, curr_steering_angle
-
-def compute_steering_angle(frame, lane_lines):
-    """ Find the steering angle based on lane line coordinate
-        We assume that camera is calibrated to point to dead center
-    """
-    if len(lane_lines) == 0:
-        logging.info('No lane lines detected, do nothing')
-        return 90
-
-    height, width, _ = frame.shape
-    if len(lane_lines) == 1:
-        x1, _, x2, _ = lane_lines[0][0]
-        x_offset = x2 - x1
+        left_cx, left_cy = 0, 0
+    
+    if (right_moment["m00"] != 0):
+        # Get the center x.
+        right_cx = int(right_moment["m10"]/right_moment["m00"])
+        # Get the center y.
+        right_cy = int(right_moment["m01"]/right_moment["m00"])
     else:
-        _, _, left_x2, _ = lane_lines[0][0]
-        _, _, right_x2, _ = lane_lines[1][0]
-        # camera_mid_offset_percent = 0.02 # 0.0 means car pointing to center, -0.03: car is centered to left, +0.03 means car pointing to right
-        camera_mid_offset_percent = 0.00
-        mid = int(width / 2 * (1 + camera_mid_offset_percent))
-        x_offset = (left_x2 + right_x2) / 2 - mid
+        right_cx, right_cy = width, 0
 
-    # find the steering angle, which is angle between navigation direction to end of center line
-    y_offset = int(height / 2)
+    cv2.circle(frame, (left_cx, left_cy), 5, (0, 0, 255), 3)
+    cv2.circle(frame, (right_cx, right_cy), 5, (0, 0, 255), 3)
 
-    angle_to_mid_radian = math.atan(x_offset / y_offset)  # angle (in radian) to center vertical line
-    angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)  # angle (in degrees) to center vertical line
-    steering_angle = angle_to_mid_deg + 90  # this is the steering angle needed by picar front wheel
+    if(len(left_box) > 0):           #This line is needed to drawContours the box
+        cv2.drawContours(frame, [left_box], -1, (0, 0, 255), 3) 
+    if(len(right_box) > 0):         #This line is needed to drawContours the box
+        cv2.drawContours(frame, [right_box], -1, (0, 0, 255), 3) 
+    
+    # if(box_center >= 2):
+    #     center_pt = int((box_center[0][0] + box_center[1][0])/2)
+    # else:
+    #     if(box_center[0][0] >= width):
+    #         center_pt = int((box_center[0][0] + width)/2)
+    #     else:
+    #         center_pt = int((box_center[0][0] + width)/2)
+        
+    # cv2.circle(frame, (center_pt, cy), 5, (0, 0, 255), 3)
 
-    return steering_angle
-
-
-def stabilize_steering_angle(curr_steering_angle, new_steering_angle, num_of_lane_lines, max_angle_deviation_two_lines=5, max_angle_deviation_one_lane=1):
-    """
-    Using last steering angle to stabilize the steering angle
-    This can be improved to use last N angles, etc
-    if new angle is too different from current angle, only turn by max_angle_deviation degrees
-    """
-    if num_of_lane_lines == 2 :
-        # if both lane lines detected, then we can deviate more
-        max_angle_deviation = max_angle_deviation_two_lines
-    else :
-        # if only one lane detected, don't deviate too much
-        max_angle_deviation = max_angle_deviation_one_lane
-
-    angle_deviation = new_steering_angle - curr_steering_angle
-    if abs(angle_deviation) > max_angle_deviation:
-        stabilized_steering_angle = int(curr_steering_angle
-                                        + max_angle_deviation * angle_deviation / abs(angle_deviation))
-    else:
-        stabilized_steering_angle = new_steering_angle
-    return stabilized_steering_angle
-
-
-############################
-# Utility Functions
-############################
-def display_lines(frame, lines, line_color=(0, 255, 0), line_width=10):
-    line_image = np.zeros_like(frame)
-    if lines is not None:
-        for line in lines:
-            for x1, y1, x2, y2 in line:
-                cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
-    line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-    return line_image
-
-
-def display_heading_line(frame, steering_angle, line_color=(0, 0, 255), line_width=5, ):
-    heading_image = np.zeros_like(frame)
-    height, width, _ = frame.shape
-
-    # figure out the heading line from steering angle
-    # heading line (x1,y1) is always center bottom of the screen
-    # (x2, y2) requires a bit of trigonometry
-
-    # Note: the steering angle of:
-    # 0-89 degree: turn left
-    # 90 degree: going straight
-    # 91-180 degree: turn right
-    steering_angle_radian = steering_angle / 180.0 * math.pi
-    x1 = int(width / 2)
-    y1 = height
-    x2 = int(x1 - height / 2 / math.tan(steering_angle_radian))
-    y2 = int(height / 2)
-
-    cv2.line(heading_image, (x1, y1), (x2, y2), line_color, line_width)
-    heading_image = cv2.addWeighted(frame, 0.8, heading_image, 1, 1)
-
-    return heading_image
-
-
-
+    cv2.imshow('test',frame)
 
 
 
@@ -432,7 +222,7 @@ def test_video(video_file):
         ret, frame = cap.read()
         if ret == True:
             # contour_image = detecting_contour(frame)
-            averaged_lines, lane_lines_image = detect_lane(frame)
+            # averaged_lines, lane_lines_image = detect_lane(frame)
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             canny_image = detecting_edges(frame)
             cropped_image = region_of_interest(canny_image)
@@ -444,7 +234,7 @@ def test_video(video_file):
             
             # lines = detect_line_segments(cropped_image)
             # averaged_lines = average_slope_intercept_middle_line(frame, lines)
-            final_image, curr_angle = steer(lane_lines_image, averaged_lines, curr_angle)
+            # final_image, curr_angle = steer(lane_lines_image, averaged_lines, curr_angle)
             # print(curr_angle)
             # transfer_data(curr_angle)
             # cv2.imshow("result", hsv)
@@ -478,14 +268,14 @@ def test_photo(photo_file):
     cropped_image = region_of_interest(canny_image)
     box_dim = find_contours(cropped_image, lane_image)
     # print(box_dim)
-    # box_center = calc_midpoints(box_dim, lane_image)
     left_box, right_box = sort_contours(box_dim, lane_image)
+    calc_midpoints(left_box, right_box, lane_image)
 
     # detecting_contour(cropped_image, lane_image)
     #canny_image = detecting_edges_grayscale(lane_image)
     
-    averaged_lines, lane_lines_image = detect_lane(lane_image)
-    final_image, curr_angle = steer(lane_lines_image, averaged_lines, curr_angle)
+    # averaged_lines, lane_lines_image = detect_lane(lane_image)
+    # final_image, curr_angle = steer(lane_lines_image, averaged_lines, curr_angle)
 
     # cv2.imshow("Original image", lane_image)
     cv2.imshow("hsv image", hsv)
@@ -525,9 +315,9 @@ def test_photo(photo_file):
 # test_photo('Drawing.jpeg')
 # test_photo('autodraw 8_25_2020.png')
 # test_video('/home/kan/Videos/SpeedLimitTestSuccess2.m4v')
-# test_photo('blue_lane.jpg')
+test_photo('blue_lane.jpg')
 # test_photo('blue_lane_2.jpg')
 # test_photo('blue_lane_3.jpg')
 # test_photo('blue_lane_4.jpg')
 # test_photo('blue_lane_5.jpg')
-test_video('Curved_lane.mp4')
+# test_video('Curved_lane.mp4')
