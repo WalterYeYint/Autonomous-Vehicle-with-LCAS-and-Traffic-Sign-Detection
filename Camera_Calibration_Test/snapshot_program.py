d9046ac2bf360c186d2801e2172e0ef9412de601
Kan Yu <WalterYeYint@gmail.com>
2020-12-02 18:36:23 +0630

    Successfully took snapshots every 5 seconds using threading while showing video. (Deleted unnecessary pictures)

--- Camera_Calibration_Test/snapshot_program.py
+++ Camera_Calibration_Test/snapshot_program.py
@@ -2,12 +2,29 @@
 
 import cv2
 from imutils.video import WebcamVideoStream
+import time
+import threading
 
 cam = WebcamVideoStream(src="https://192.168.1.103:8080/video").start()
 
 cv2.namedWindow("test")
 
 img_counter = 0
+frame = cam.read()
+
+def snapshot_thread():
+    while True:
+        global img_counter
+        ### Take snapshots periodically
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+        time.sleep(5)
+
+
+t = threading.Thread(target=snapshot_thread, name='snapshot_thread', args=())
+t.start()
 
 while True:
     frame = cam.read()
@@ -18,12 +35,14 @@ while True:
         # ESC pressed
         print("Escape hit, closing...")
         break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
+
+    ### Take snapshots by typing spacebar
+    # elif k%256 == 32:
+    #     # SPACE pressed
+    #     img_name = "opencv_frame_{}.png".format(img_counter)
+    #     cv2.imwrite(img_name, frame)
+    #     print("{} written!".format(img_name))
+    #     img_counter += 1
 
 cam.release()
 
